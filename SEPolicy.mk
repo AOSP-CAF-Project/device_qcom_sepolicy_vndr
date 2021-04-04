07456baeab5ca058885d3eaa468a5e2d30730fb9
Mohd Faraz <androiabledroid@gmail.com>
2021-04-04 13:22:24 +0200

    sepolicy: Added sdm660 policies from https://source.codeaurora.org/quic/la/device/qcom/sepolicy/tree/?h=LA.UM.9.2.1.r1-06600-sdm660.0
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>
    Change-Id: Ief7cc92c30061d5ddbc23e57ddf2544cb81c4fc7

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -33,7 +33,7 @@ PRODUCT_PRIVATE_SEPOLICY_DIRS := \
     $(QSSI_SEPOLICY_PATH)/generic/product/private \
     $(QSSI_SEPOLICY_PATH)/qva/product/private
 
-ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
+ifeq (,$(filter sdm660 sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
     BOARD_SEPOLICY_DIRS := \
        $(BOARD_SEPOLICY_DIRS) \
        $(SEPOLICY_PATH) \
@@ -58,7 +58,7 @@ ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
     endif
 endif
 
-ifneq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter sdm660 sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
     BOARD_SEPOLICY_DIRS := \
                  $(BOARD_SEPOLICY_DIRS) \
                  $(SEPOLICY_PATH) \
